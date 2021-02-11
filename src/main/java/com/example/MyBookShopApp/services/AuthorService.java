ce15883d730e5f31984408a3b07f570b01f63606
Wizzard-sudo <kolia.makarevich.98@gmail.com>
2021-02-11 13:36:03 +0300

    small restructuring

--- src/main/java/com/example/MyBookShopApp/services/AuthorService.java
+++ src/main/java/com/example/MyBookShopApp/services/AuthorService.java
@@ -1,4 +1,4 @@
-package com.example.MyBookShopApp.data;
+package com.example.MyBookShopApp.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
